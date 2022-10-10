4f9bf0f27713aa746cbc7deb6816e3febb5d1591
Serhii Kavierin <serhiikavierin@gmail.com>
2022-10-10 23:05:18 +0300

    Initial

---
+++ dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.9
+
+RUN mkdir /app
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install -r requirements.txt
+
+ENTRYPOINT [ "python", "main.py" ]
---
+++ main.py
@@ -0,0 +1,33 @@
+import logging
+import sys
+from google.cloud import storage
+from datetime import datetime
+
+from config import BUCKET_NAME
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    gcs_client = storage.Client()
+
+    current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+
+    bucket = gcs_client.get_bucket(BUCKET_NAME))
+    blob = bucket.blob(f'results/{current_time}.txt')
+
+    blob.upload_from_string(
+        'sample_text', content_type='application/x-www-form-urlencoded;charset=UTF-8')
+
+    logger.info(f'File created in bucket {bucket}, at path {blob}')
+
+    sys.exit(0)
